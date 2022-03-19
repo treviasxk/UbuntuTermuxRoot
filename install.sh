f1b4875a0d6df85567861e8c00ba3576a7140a7e
Trevias Xk <trevias@live.com>
2022-03-19 14:51:58 -0300

    • Revertido e melhorado instalação.

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 rm install
-if [ "$EUID" -ne 0 ]
-  then echo "Por favor execute com o modo root"
-  exit
-fi
 banner (){
     clear
     echo " ============= UBUNTU TERMUX ROOT ============="
@@ -18,48 +14,57 @@ banner (){
 }
 banner
 echo -e "\e[30;48;5;82m STATUS \e[40;38;5;82m BAIXANDO... \e[0m"
+apt update
+apt install tsu -y
+apt install xz-utils -y
+apt install wget -y
 wget https://cdimage.ubuntu.com/ubuntu-base/releases/21.10/release/ubuntu-base-21.10-base-arm64.tar.gz
 banner
 echo -e "\e[30;48;5;82m STATUS \e[40;38;5;82m INSTALANDO... \e[0m"
 
+sudo mkdir -p /data/local/ubuntu 
+sudo tar -xzf ./ubuntu-base-21.10-base-arm64.tar.gz --exclude='dev' -C /data/local/ubuntu
+rm ./ubuntu-base-21.10-base-arm64.tar.gz
+
 mount -o rw,remount /data
 mount -o rw,remount /system/bin
 
-mkdir -p /data/local/ubuntu 
-tar -xzf ./ubuntu-base-21.10-base-arm64.tar.gz --exclude='dev' -C /data/local/ubuntu
-rm ./ubuntu-base-21.10-base-arm64.tar.gz
+echo "nameserver 8.8.8.8" > ./resolv.conf
+echo "nameserver 8.8.4.4" >> ./resolv.conf
+sudo mv ./resolv.conf /data/local/ubuntu/etc/resolv.conf
+sudo chmod 644 /data/local/ubuntu/etc/resolv.conf
 
+echo "groupadd -g 3003 aid_inet" > ./finalizar
+echo "usermod -a -G aid_inet root" >> ./finalizar
+echo "adduser --force-badname --system --home /nonexistent --no-create-home --quiet _apt || true" >> ./finalizar
+echo "usermod -g 3003 _apt" >> ./finalizar
+echo 'echo -e "\e[30;48;5;82m STATUS \e[40;38;5;82m INSTALAÇÃO FINALIZADO! \e[0m"' >> ./finalizar
+echo 'rm ./finalizar' >> ./finalizar
+sudo mv ./finalizar /data/local/ubuntu/root
+sudo chmod +x /data/local/ubuntu/root/finalizar 
 
-echo "nameserver 8.8.8.8" > /data/local/ubuntu/etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /data/local/ubuntu/etc/resolv.conf
-chmod 644 /data/local/ubuntu/etc/resolv.conf
-
-echo "groupadd -g 3003 aid_inet" > /data/local/ubuntu/root/finalizar
-echo "usermod -a -G aid_inet root" >> /data/local/ubuntu/root/finalizar
-echo "adduser --force-badname --system --home /nonexistent --no-create-home --quiet _apt || true" >> /data/local/ubuntu/root/finalizar
-echo "usermod -g 3003 _apt" >> /data/local/ubuntu/root/finalizar
-echo 'echo -e "\e[30;48;5;82m STATUS \e[40;38;5;82m INSTALAÇÃO FINALIZADO! \e[0m"' >> /data/local/ubuntu/root/finalizar
-echo 'rm /data/local/ubuntu/root/finalizar' >> /data/local/ubuntu/root/finalizar
-chmod +x /data/local/ubuntu/root/finalizar 
-
-mkdir -p /data/local/ubuntu/dev
-echo "127.0.0.1 localhost" > /data/local/ubuntu/etc/hosts
-echo "::1   localhost.localdomain" >> /data/local/ubuntu/etc/hosts
+sudo mkdir -p /data/local/ubuntu/dev
+echo "127.0.0.1 localhost" > ./hosts
+echo "::1   localhost.localdomain" >> ./hosts
+sudo mv ./hosts /data/local/ubuntu/etc/hosts
 
 cd
-echo "unset LD_PRELOAD" > ./ubuntu
-echo "clear" >> ./ubuntu
+echo "unset LD_PRELOAD" > ../usr/bin/ubuntu
+echo "clear" >> ../usr/bin/ubuntu
+echo "export bin=/system/bin" >> ../usr/bin/ubuntu
+echo 'export PATH=/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:$PATH' >> ../usr/bin/ubuntu
+echo "export TERM=linux" >> ../usr/bin/ubuntu
+echo "export HOME=/root" >> ../usr/bin/ubuntu
+echo "export USER=root" >> ../usr/bin/ubuntu
+echo "export LOGNAME=root" >> ../usr/bin/ubuntu
+cp ../usr/bin/ubuntu ./ubuntu
 echo "setenforce 0" >> ./ubuntu
-echo "export bin=/system/bin" >> ./ubuntu
-echo 'export PATH=/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:$PATH' >> ./ubuntu
-echo "export TERM=linux" >> ./ubuntu
-echo "export HOME=/root" >> ./ubuntu
-echo "export USER=root" >> ./ubuntu
-echo "export LOGNAME=root" >> ./ubuntu
 echo "busybox chroot /data/local/ubuntu /bin/login -f root" >> ./ubuntu
-chmod 777 ./ubuntu
-cp ./ubuntu /system/bin
-cp ./ubuntu /data/data/com.termux/files/usr/bin
+mv ./ubuntu /system/bin
+sudo chmod 777 /system/bin
+echo "sudo setenforce 0" >> ../usr/bin/ubuntu
+echo "sudo busybox chroot /data/local/ubuntu /bin/login -f root" >> ../usr/bin/ubuntu
+chmod 777 ../usr/bin/ubuntu
 
 banner
 echo -e "\e[30;48;5;82m STATUS \e[40;38;5;82m INSTALADO COM SUCESSO! \e[0m"
