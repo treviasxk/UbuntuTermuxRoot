6e5fe9ad2337a24b49741687ddb84cde7ecdb36c
Trevias Xk <trevias@live.com>
2022-03-19 11:24:09 -0300

    • Primeira versão estável.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ clear
     echo "   ||   |  \ |___   \/   | /   \  __|  _/\_ |\_"
     echo -e "\e[0m\e[39m ______________________________________________"
     echo " REDES SOCIAIS:                       treviasxk"
-    echo " VERSÃO:                              1.0.1.1"
+    echo " VERSÃO:                              1.0.0.0"
     echo " LICENÇA:                             GPL-3.0"
     echo " =============================================="
 echo -e "\e[30;48;5;82m STATUS \e[40;38;5;82m BAIXANDO! \e[0m"
@@ -20,18 +20,42 @@ echo -e "\e[30;48;5;82m STATUS \e[40;38;5;82m INSTALANDO! \e[0m"
 
 sudo mkdir -p /data/local/ubuntu 
 sudo tar -xzf ./ubuntu-base-21.10-base-arm64.tar.gz --exclude='dev' -C /data/local/ubuntu
+rm ./ubuntu-base-21.10-base-arm64.tar.gz
 
 #mount -o rw,remount /
 #mount -o rw,remount /system
 
-sudo echo "nameserver 8.8.8.8" > /data/local/ubuntu/etc/resolv.conf
-sudo echo "nameserver 8.8.4.4" >> /data/local/ubuntu/etc/resolv.conf
+echo "nameserver 8.8.8.8" > ./resolv.conf
+echo "nameserver 8.8.4.4" >> /data/local/ubuntu/etc/resolv.conf
+sudo mv ./resolv.conf /data/local/ubuntu/etc/resolv.conf
 sudo chmod 644 /data/local/ubuntu/etc/resolv.conf
+
 sudo mkdir -p /data/local/ubuntu/dev
-sudo echo "127.0.0.1 localhost" > /data/local/ubuntu/etc/hosts
-sudo echo "::1   localhost.localdomain" >> /data/local/ubuntu/etc/hosts
+echo "127.0.0.1 localhost" > ./hosts
+echo "::1   localhost.localdomain" >> ./hosts
+sudo mv ./hosts /data/local/ubuntu/etc/hosts
 
 cd
-echo "sudo busybox chroot /data/local/ubuntu /bin/login -f root" > ../usr/bin/ubuntu
+echo "unset LD_PRELOAD" > ../usr/bin/ubuntu
+echo "clear" >> ../usr/bin/ubuntu
+echo "setenforce 0" >> ../usr/bin/ubuntu
+echo "export bin=/system/bin" >> ../usr/bin/ubuntu
+echo 'export PATH=/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:$PATH' >> ../usr/bin/ubuntu
+echo "export TERM=linux" >> ../usr/bin/ubuntu
+echo "export HOME=/root" >> ../usr/bin/ubuntu
+echo "export USER=root" >> ../usr/bin/ubuntu
+echo "export LOGNAME=root" >> ../usr/bin/ubuntu
+echo "sudo busybox chroot /data/local/ubuntu /bin/login -f root" >> ../usr/bin/ubuntu
+
 chmod 777 ../usr/bin/ubuntu
+clear
+    echo " ============= UBUNTU TERMUX ROOT ============="
+    echo -e "\e[1m\e[32m ______  ___  ___ _    _ .   _   ___   _  _ _ _"
+    echo "   ||   |__/ |___  \  /  |  /_\  |__    \/  |/ "
+    echo "   ||   |  \ |___   \/   | /   \  __|  _/\_ |\_"
+    echo -e "\e[0m\e[39m ______________________________________________"
+    echo " REDES SOCIAIS:                       treviasxk"
+    echo " VERSÃO:                              1.0.0.0"
+    echo " LICENÇA:                             GPL-3.0"
+    echo " =============================================="
 echo -e "\e[30;48;5;82m STATUS \e[40;38;5;82m INSTALADO COM SUCESSO! \e[0m"
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Ubuntu Termux Root
-Instale e execute o Ubuntu Base na raiz do seu smartphone com o termux.
+Instale e execute o Ubuntu Base na raiz do seu smartphone com o termux. Utilize o comando `ubuntu` para poder iniciar o sistema depois da instalação.
 ## Instalação
 Insira o comando bash abaixo no Termux para poder fazer a instalação.
 ```bash
